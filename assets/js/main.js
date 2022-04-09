2b531bbd209ff895858c0964bab9a76b304e8498
badr ghanbi <bghanbi50@gmail.com>
2022-04-09 23:24:52 +0000

    home and about section + add animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
 /*=============== SHOW MENU ===============*/
+const header=document.querySelector('header');
+const nav=document.querySelector('.nav');
 const navCollapsible=document.querySelector('.nav__collapsible');
 const hamburger=document.querySelector('.nav__hamburger');
 
 function showMenu(){
-    navCollapsible.classList.toggle('showList')
+    navCollapsible.classList.toggle('showList');
+    header.classList.toggle('expandheader');
 }
-
+function fixNav(){
+if(window.scrollY>0) nav.classList.add('fixNav');
+else nav.classList.remove('fixNav');
+}
+window.addEventListener('scroll',fixNav)
 hamburger.addEventListener('click',showMenu);
-/*=============== REMOVE MENU MOBILE ===============*/
-
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
