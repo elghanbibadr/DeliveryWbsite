6a9af58c946fe5351e526b0f74fc6efac6fabf4b
badr ghanbi <bghanbi50@gmail.com>
2022-04-09 18:11:51 +0000

    navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 /*=============== SHOW MENU ===============*/
+const navCollapsible=document.querySelector('.nav__collapsible');
+const hamburger=document.querySelector('.nav__hamburger');
 
+function showMenu(){
+    navCollapsible.classList.toggle('showList')
+}
 
+hamburger.addEventListener('click',showMenu);
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
